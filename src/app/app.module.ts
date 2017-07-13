ec9ee46c2dd8cafc493dcc52b24cadc4b5739ab2
Joel Dinis <joeldinisei1@gmail.com>
2017-07-13 02:41:59 +0200

    More stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { Geolocation } from '@ionic-native/geolocation';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -34,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+
+declare var google;
 
 @Component({
   selector: 'page-about',
@@ -7,8 +11,34 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+   @ViewChild('map') mapElement;
+   map: any;
 
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+ 
+  }
+
+  ionViewDidLoad(){
+  		this.initMap();
+
+  }
+
+  initMap(){
+         this.geolocation.getCurrentPosition().then((position) => {
+         
+let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+  		let mapOptions = {
+  				center: latLng,
+  				zoom: 15,
+  				mapTypeId: google.maps.MapTypeId.ROADMAP
+
+  		};
+
+  		this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+}, (err) => {
+      console.log(err);
+    });
   }
 
 }
