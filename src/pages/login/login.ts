93d832e23d19a498b1d741a8441c569b3eb6a34b
Joel Dinis <joeldinisei1@gmail.com>
2017-07-14 16:11:37 +0200

    Fica mais bonito assim
    https://devdactic.com/login-ionic-2/

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,17 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController} from 'ionic-angular';
+import { NavController, LoadingController, Loading} from 'ionic-angular';
 
 @Component({
   templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {
+	loading: Loading;
+	
+  constructor(public navCtrl: NavController, private loadingCtrl: LoadingController) {
 
   }
 
-   logEvent(event) {
-    console.log(event);
+   public login() {
+    this.showLoading();
+   /* this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if (allowed) {        
+        this.nav.setRoot('HomePage');
+      } else {
+        this.showError("Access Denied");
+      }
+    },
+      error => {
+        this.showError(error);
+      });
+      */
+  }
+
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
   }
 
 }
