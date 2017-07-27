fd214cbf31f99d66e2edb8c8c677483e45260d3a
Joel Dinis <joeldinisei1@gmail.com>
2017-07-27 17:44:43 +0200

    test

---
+++ src/pages/home/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('app',['ionic']);
+app.controller('Controlador', function($scope){
+	
+$scope.model={
+	"contactos":[{
+		"nombre":"christian"
+	},
+	{
+	"nombre": "christian2",
+	}]
+
+};
+
+});
