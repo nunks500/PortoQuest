02fb6f8a07f312ef32cc0ddb8443e7ca50ade729
Joel Dinis <joeldinisei1@gmail.com>
2017-07-28 12:38:46 +0200

    nice stuff

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	  posts: any;
+	  html;
 
-  }
+  constructor(public navCtrl: NavController, public http: Http) {
+  	   this.http.get('https://porto-quest.herokuapp.com/api/getlatobj').map(res => res.json()).subscribe(data => {
+        this.posts = data;
+        console.log(this.posts);
+        console.log(data);
+        this.posts.nome = data[0].nome;
+        this.html = data[0].nome;
 
+  });
+
+}
 }
